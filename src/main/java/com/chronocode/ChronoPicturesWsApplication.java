d7607d124b1583e9f65079073910098bbba41401
RunnersReign <domagals.jame@gmail.com>
2016-09-14 23:35:37 -0500

    Initial Commit

---
+++ src/main/java/com/chronocode/ChronoPicturesWsApplication.java
@@ -0,0 +1,12 @@
+package com.chronocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronoPicturesWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronoPicturesWsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/chronocode/ChronoPicturesWsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chronocode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChronoPicturesWsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
